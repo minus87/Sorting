0e85b4eafa84e016484ac29c3ca5e6a5219ec6d1
Wang Yu <43696104+minus87@users.noreply.github.com>
2018-10-10 16:09:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
-/* Project: Sorting, File: main.cpp */
+/* Project: Sorting
+ * File: main.cpp
+ * Author: Yu Wang
+ * Date: Oct, 2018 */
 
-#include <iostearm>
-using namespace std
+#include <iostream>
+#include "sorting.h"
 
 int main()
-{
-	
+{	
+	struct RunTime {
+		double insert_sort;
+		double shell_sort;
+		double heap_sort;
+		double merge_sort;
+		double quick_sort;
+	}run_time;
+	float *data;
+	int num_of_data = 10000;
+
+	/* Create the 10K float point numbers for sorting */
+	data = generate_float_data(num_of_data);
+
+	/* sorting with different methods and runturn their running times */
+	run_time.insert_sort = insertion_sort(data, num_of_data);
+	run_time.shell_sort = shell_sort(data, num_of_data);
+	run_time.heap_sort = heap_sort(data, num_of_data);
+	run_time.merge_sort = merge_sort(data, num_of_data);
+	run_time.quick_sort = quick_sort(data, num_of_data);
+
+	/* display the running times of the different sorting methods */
+	cout << "Running time:" << endl;
+	cout << "* Insertion sort:\t" << run_time.insert_sort << "s" << endl;
+	cout << "* Shell sort:\t" << run_time.insert_sort << "s" << endl;
+	cout << "* Heap sort:\t" << run_time.insert_sort << "s" << endl;
+	cout << "* Merge sort:\t" << run_time.insert_sort << "s" << endl;
+	cout << "* Quick sort:\t" << run_time.insert_sort << "s" << endl;
+
+	return 1;
 }
